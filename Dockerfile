4935ecb0efb3b68d11e4fde019f0998878e8d3ae
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-07-23 12:09:23 +0200

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM bentoml/model-server:0.11.0-py37
 MAINTAINER ersilia
 
-RUN pip install rdkit
+RUN pip install unimol_tools==0.1.0
+RUN pip install huggingface_hub==0.24.0
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -1,39 +1,32 @@
-# imports
 import os
 import csv
 import sys
-from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+import numpy as np
+from unimol_tools import UniMolRepr
 
-# parse arguments
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
-# current file directory
 root = os.path.dirname(os.path.abspath(__file__))
 
-# my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
-
-
-# read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
     reader = csv.reader(f)
-    next(reader)  # skip header
+    next(reader)
     smiles_list = [r[0] for r in reader]
 
-# run model
-outputs = my_model(smiles_list)
+clf = UniMolRepr(data_type='molecule', remove_hs=False, use_gpu=False)
+unimol_repr = clf.get_repr(smiles_list, return_atomic_reprs=False)
 
-#check input and output have the same lenght
+X = np.array(unimol_repr['cls_repr'])
+
+header = ["feat-{0}".format(i) for i in X.shape[1]]
+
 input_len = len(smiles_list)
-output_len = len(outputs)
+output_len = X.shape[0]
 assert input_len == output_len
 
-# write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
-    for o in outputs:
-        writer.writerow([o])
+    writer.writerow(header)
+    for i in range(X.shape[0]):
+        writer.writerow(list(X[i,:]))
