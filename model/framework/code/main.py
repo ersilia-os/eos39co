2f564a3a003501a6285271bb4e21daf190b17049
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2024-07-23 13:00:21 +0200

    updating readme [skip ci]

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -19,7 +19,7 @@ unimol_repr = clf.get_repr(smiles_list, return_atomic_reprs=False)
 
 X = np.array(unimol_repr['cls_repr'])
 
-header = ["feat-{0}".format(i) for i in X.shape[1]]
+header = ["feat-{0}".format(i) for i in range(X.shape[1])]
 
 input_len = len(smiles_list)
 output_len = X.shape[0]
