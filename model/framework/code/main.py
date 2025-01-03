97a2a2d870047db919126a55885e83a7debba0b2
Miquel Duran-Frigola <miquelduranfrigola@gmail.com>
2025-01-03 15:52:32 +0100

    added columns

--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -19,7 +19,7 @@ unimol_repr = clf.get_repr(smiles_list, return_atomic_reprs=False)
 
 X = np.array(unimol_repr['cls_repr'])
 
-header = ["feat-{0}".format(i) for i in range(X.shape[1])]
+header = ["dimension_{0}".format(str(i).zfill(3)) for i in range(X.shape[1])]
 
 input_len = len(smiles_list)
 output_len = X.shape[0]
